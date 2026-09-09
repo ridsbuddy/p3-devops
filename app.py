286a9e465d4423ba5a77dd5613e2fc930b128c6b
ridsbuddy <csaiml21164@glbitm.ac.in>
2026-09-09 16:29:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,21 @@ DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
- 
+
+
 def get_db_connection():
-  return psycopg2.connect(
-    host=DB_HOST,
-    database=DB_NAME,
-    user=DB_USER,
-    password=DB_PASSWORD
-)
+    return psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+
 
 @app.route("/")
 def home():
     return {"application": "Book Inventory API"}
+
 
 @app.route("/books")
 def books():
@@ -27,7 +30,6 @@ def books():
     cur = conn.cursor()
 
     cur.execute("SELECT id, title FROM books")
-
     rows = cur.fetchall()
 
     cur.close()
@@ -41,9 +43,11 @@ def books():
         for row in rows
     ]
 
+
 @app.route("/health")
 def health():
     return {"status": "healthy"}
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
