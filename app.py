55582b739b9835511ed20527973e70d18cf3dd93
ridsbuddy <csaiml21164@glbitm.ac.in>
2026-09-09 15:45:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+import os
+import psycopg2
 from flask import Flask
 
 app = Flask(__name__)
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+ 
+def get_db_connection():
+return psycopg2.connect(
+host=DB_HOST,
+database=DB_NAME,
+user=DB_USER,
+password=DB_PASSWORD
+)
 
 @app.route("/")
 def home():
